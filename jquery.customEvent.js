ed1d5b86b9344389595195852a9fe64a07be6560
bbd33 <damardotnet@gmail.com>
2013-02-13 02:13:16 +0700

    Update jquery.customEvent.js

--- jquery.customEvent.js
+++ jquery.customEvent.js
@@ -1,15 +1,16 @@
-$.removeCustomEvent = function(elem,eventName){
-  	elem = $(elem);
-		var customEventAttr = 'customEventKey_'+eventName ;
-		var customEventKey = elem.attr(customEventAttr);
-		if( typeof window.customEvent[customEventKey] !== 'undefined'){
-			window.customEvent[customEventKey] = false;
-		}
-	}
+	/*
+	@filename jquery.customEvent.js
+	@author bbd33
+	@mail damardotnet@gmail.com
+	@site https://github.com/bbd33/jquery-plugin
+	*/
+	
+	// Add Custom event to HTML element then fired 
+	// by state of the specified user function 
+	
+
 	$.addCustomEvent = function(elem,eventName,timeout,fnState){
-		if( typeof elem === 'string' ){
-			elem = $(elem).get(0);
-		}
+		elem = $(elem);
 		if( !$(elem).length )
 			return;
 		if( typeof window.customEvent === 'undefined' ){
@@ -31,25 +32,43 @@ $.removeCustomEvent = function(elem,eventName){
 		}
 		
 		eventLoop();
+	};
+	// Remove Custome event for being fired again
+	
+	$.removeCustomEvent = function(elem,eventName){
+  		elem = $(elem);
+		var customEventAttr = 'customEventKey_'+eventName ;
+		var customEventKey = elem.attr(customEventAttr);
+		if( typeof window.customEvent[customEventKey] !== 'undefined'){
+			window.customEvent[customEventKey] = false;
+		}
 	}
 	
-	$('input#file_id').livequery(function(){
-		$.addCustomEvent( 'input#file_id','dataChanged',500,function(elem){
-			elem = $(elem);
-			var hasOldValue = $(elem).attr('hasOldValue');
-			var firstInit = false;
-			
-			if( !hasOldValue ){
-				firstInit = true;	
-				$(elem).attr('hasOldValue',true);
-			}
-			
-			var oldValue = $(elem).data('oldValue');
-			var currentValue = $(elem).val();
-			$(elem).data('oldValue',currentValue);
-			return (oldValue !== currentValue) || firstInit;
-			
-			//console.log(elem.val());
-		});
-		$('input#file_id').bind('dataChanged',function(){console.log($(this).val())})
+	// Example Initialization
+	// Add event name called "dataChenged"
+	
+	$.addCustomEvent( 'input#file_id','dataChanged',500,function(elem){
+		elem = $(elem);
+		var hasOldValue = $(elem).attr('hasOldValue');
+		var firstInit = false;
+		
+		if( !hasOldValue ){
+			firstInit = true;	
+			$(elem).attr('hasOldValue',true);
+		}
+		
+		var oldValue = $(elem).data('oldValue');
+		var currentValue = $(elem).val();
+		$(elem).data('oldValue',currentValue);
+		return (oldValue !== currentValue) || firstInit;
+		
+		//console.log(elem.val());
 	});
+	
+	// Add event handler for recently created custom event 
+	// called "dataChenged"
+	
+	$('input#file_id').bind('dataChanged',function(){
+		console.log($(this).val());
+	});
+
